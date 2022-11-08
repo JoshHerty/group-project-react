40c2590fb7cde47009944d44b257aedc2f1378e7
Brendan Chico <boomaster40@gmail.com>
2022-11-08 13:06:57 -0500

    CSS work

--- src/components/DetailedResult.tsx
+++ src/components/DetailedResult.tsx
@@ -15,7 +15,7 @@ const DetailedResult = ({ detailedRecipe }: Props) => {
     <div className="DetailedResult">
       <p>{detailedRecipe.title}</p>
       <img src={detailedRecipe.image} alt={detailedRecipe.title} />
-      <p>{cleanSummary}</p>
+      <p className="summary">{cleanSummary}</p>
       <a href={detailedRecipe.sourceUrl}>Source</a>
       {!isFav(detailedRecipe.id) ? (
         <button
